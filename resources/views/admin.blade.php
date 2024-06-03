ba86c46007f8520f9b0dc64e1add7ff1dda29640
Sasha <sasharotaaenko1@gmail.com>
2024-06-03 18:26:35 +0300

    work before dish type function

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,21 +6,21 @@
 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 	
 
-	<!-- Import lib -->
+	
 
 	<link rel="stylesheet" type="text/css" href="{{url('CSS/admin.css')}}">
 
 	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/e3a49d370f.js" crossorigin="anonymous"></script>
 	
-    <!-- End import lib -->
+    <
 
 	
 </head>
 <body class="overlay-scrollbar bg ">
-	<!-- navbar -->
+	
 	<div class="navbar">
-		<!-- nav left -->
+		
 		<ul class="navbar-nav">
 			<li class="nav-item">
 				<a class="nav-link">
@@ -33,13 +33,10 @@
 				</div>
 			</li>
 		</ul>
-		<!-- end nav left -->
-		<!-- form -->
+		
 		<form class="navbar-search">
 			
 		</form>
-		<!-- end form -->
-		<!-- nav right -->
 		<ul class="navbar-nav nav-right">
 			<li class="nav-item mode">
 				<a class="nav-link" href="#" onclick="switchTheme()">
@@ -56,7 +53,7 @@
 								<div>
 									<i class="fas fa-user-tie"></i>
 								</div>
-								<span>{{ Auth::user()->name }}</span> <!-- Display user's name -->
+								<span>{{ Auth::user()->name }}</span> 
 							</a>
 						</li>
 						<li class="dropdown-menu-item">
@@ -76,10 +73,8 @@
 				</div>
 			</li>
 		</ul>
-		<!-- end nav right -->
+
 	</div>
-	<!-- end navbar -->
-	<!-- sidebar -->
 	<div class="sidebar">
 		<ul class="sidebar-nav">
 			<li class="sidebar-nav-item">
@@ -116,17 +111,12 @@
 			</li>
 		</ul>
 	</div>
-	<!-- end sidebar -->
-	<!-- main content -->
     <div class="wrapper">
 	@yield('content')
 
   
     </div>
-	<!-- end main content -->
-	<!-- import script -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 	<script src="index.js"></script>
-	<!-- end import script -->
 </body>
 </html>
